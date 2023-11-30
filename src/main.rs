0e2adf969957a1f2889ea0eca7a21e3649652f10
Richard <richardkenyon1991@hotmail.co.uk>
2023-11-30 14:03:19 +0000

    simplified version

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 use std::env;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
-
+use std::fs;
 fn main() {
     let file_name = env::args().skip(1).next().expect("Usage: cargo run -- inputfilename\n");
     
-    let file = File::open(file_name).expect("Failed to open file");
-    let mut buf_reader: BufReader<File> = BufReader::new(file);
+    let mut total = 0.0;
 
-    let mut buf = String::new();
-    let mut num_bytes = buf_reader.read_line(&mut buf).unwrap();
-    
-    let mut total = 0;
-
-    while num_bytes != 0 {
-        let num_from_line: i32 = buf.trim().parse().unwrap();
-        println!("{}", num_from_line);
-        total = total + num_from_line;
-        //println!("{buf}");
-        buf.clear();
-        num_bytes = buf_reader.read_line(&mut buf).unwrap();
+    for line in fs::read_to_string(file_name).unwrap().lines() {
+        let num: f64 = line.trim().parse().unwrap();
+        total = total + num;
     }
-    println!("Total: {total}");
+    println!("{total}");    
 }
